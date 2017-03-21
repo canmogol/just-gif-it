4ccf862bd4c0e49b4ec1f9806963d0fc6ed9f6ec
Can A. MOGOL <canmogol@gmail.com>
2017-03-21 14:10:31 +0300

    application class updated, sample configuration added to application propertoes

--- src/main/java/com/fererlab/JustGifItApplication.java
+++ src/main/java/com/fererlab/JustGifItApplication.java
@@ -1,12 +1,77 @@
 package com.fererlab;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.boot.autoconfigure.jmx.JmxAutoConfiguration;
+import org.springframework.boot.autoconfigure.websocket.WebSocketAutoConfiguration;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.filter.HttpPutFormContentFilter;
+import org.springframework.web.filter.RequestContextFilter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@SpringBootApplication
+import javax.annotation.PostConstruct;
+import java.io.File;
+
+// http://docs.spring.io/spring-boot/docs/current/reference/html/auto-configuration-classes.html
+//@EnableAutoConfiguration(exclude = {com.fererlab.controller.UploadController.class})
+//@EnableAutoConfiguration(excludeName = {"com.fererlab.controller.UploadController"})
+
+@SpringBootApplication(exclude = {JacksonAutoConfiguration.class,
+        JmxAutoConfiguration.class, WebSocketAutoConfiguration.class,
+})
 public class JustGifItApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JustGifItApplication.class, args);
-	}
+    @Value("${spring.http.multipart.location}/gif/")
+    private String gifLocation;
+
+    public static void main(String[] args) {
+        SpringApplication.run(JustGifItApplication.class, args);
+    }
+
+    @PostConstruct
+    private void init() {
+        File gifFolder = new File(gifLocation);
+        if (!gifFolder.exists()) {
+            gifFolder.mkdir();
+        }
+    }
+
+    @Bean
+    public FilterRegistrationBean deRegisterHiddenHttpFilter(HiddenHttpMethodFilter filter) {
+        FilterRegistrationBean bean = new FilterRegistrationBean(filter);
+        bean.setEnabled(false);
+        return bean;
+    }
+
+    @Bean
+    public FilterRegistrationBean deRegisterHttpPutFilter(HttpPutFormContentFilter filter) {
+        FilterRegistrationBean bean = new FilterRegistrationBean(filter);
+        bean.setEnabled(false);
+        return bean;
+    }
+
+    @Bean
+    public FilterRegistrationBean deRegisterRequestContextFilter(RequestContextFilter filter) {
+        FilterRegistrationBean bean = new FilterRegistrationBean(filter);
+        bean.setEnabled(false);
+        return bean;
+    }
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addResourceHandlers(ResourceHandlerRegistry registry) {
+                registry.addResourceHandler("/gif/**")
+                        .addResourceLocations("file:" + gifLocation);
+                super.addResourceHandlers(registry);
+            }
+        };
+    }
 }
