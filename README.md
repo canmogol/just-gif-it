d27dedee422b17e8aad1936c9dd5861504e16f07
Can A. MOGOL <canmogol@gmail.com>
2017-03-21 12:38:15 +0300

    upload controller added, sample video file and how to upload added

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # just-gif-it
-spring boot example project
+Spring boot example project
+
+## Command Line Project Creation
+`curl start.spring.io/starter.zip -o demo.zip --data @options.curl`
+see the options file for dependencies and project name, artifact id etc.
+
+## Upload Video With CURL
+curl -F file=@cats.mp4 -F start=0 -F end=0 -F speed=1 -F repeat=0 localhost:8080/upload
--- src/test/java/com/fererlab/JustGifItApplicationTests.java
+++ src/test/java/com/fererlab/JustGifItApplicationTests.java
@@ -5,12 +5,16 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+// Spring Boot 1.3 and 1.4 below configuration
+//@RunWith(SpringJUnit4ClassRunner.class)
+// SpringApplicationConfiguration is deprecated
+//@SpringApplicationConfiguration(classes = DemoApplication.class)
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class JustGifItApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
